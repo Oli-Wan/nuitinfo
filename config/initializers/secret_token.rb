46a58ae39762b674b4e9d7b4f380da077541ac14
Olivier Schneider <oliwan3dfx@gmail.com>
2012-12-03 14:38:48 +0100

    Add rails project arch

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nuitinfo::Application.config.secret_token = '33a2e9936eae6f5f1cb85a3bccf6f7ab94d2edc8d82535b81c16cbb38452efbe8bf46687d042c252057d419f4a73026fd937243a90c8bd1dde0690ae114e3096'
