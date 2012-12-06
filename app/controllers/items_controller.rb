fc41dfeb809fef5e2addf4567b1ae92d8c2e3a00
Olivier Schneider <oliwan3dfx@gmail.com>
2012-12-07 00:03:29 +0100

    Add better json view for get item/:id

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,7 +6,7 @@ class ItemsController < ApplicationController
 
     respond_to do |format|
       format.html # index.html.erb
-      format.json { render json: @items }
+      format.json { render show }
     end
   end
 
@@ -17,7 +17,7 @@ class ItemsController < ApplicationController
 
     respond_to do |format|
       format.html # show.html.erb
-      format.json { render json: @item }
+      format.json
     end
   end
 
